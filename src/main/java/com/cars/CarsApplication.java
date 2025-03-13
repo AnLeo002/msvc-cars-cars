2476a3c3429c3447f0acda4c364f82c904f220fe
Andre <lulesaaso2@gmail.com>
2025-03-13 11:17:18 -0500

    create-cars

---
+++ src/main/java/com/cars/CarsApplication.java
@@ -0,0 +1,15 @@
+package com.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsApplication.class, args);
+	}
+
+}
