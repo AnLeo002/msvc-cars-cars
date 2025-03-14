b236ad71eeb4867da227d43cc473b06a1c3b6fb5
Andre <lulesaaso2@gmail.com>
2025-03-13 19:28:46 -0500

    create-CarDTOResponse-controller/dto

---
+++ src/main/java/com/cars/controller/dto/CarsDTOResponse.java
@@ -0,0 +1,17 @@
+package com.cars.controller.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record CarsDTOResponse (@NotBlank Long id,
+                               @NotBlank String model,
+                               @NotBlank int age,
+                               @NotBlank double km,
+                               @NotBlank String color,
+                               @NotBlank String description,
+                               @NotBlank String motor,
+                               @NotBlank String price,
+                               @NotBlank String fuel,
+                               @NotBlank String brand,
+                               @NotBlank String transmission,
+                               @NotBlank String type) {
+}
