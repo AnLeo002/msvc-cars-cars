3ae30f2d9bb2e7013c363c84eaa21006d8314fc5
Andre <lulesaaso2@gmail.com>
2025-03-14 10:40:59 -0500

    create-ICarService-service

---
+++ src/main/java/com/cars/service/ICarService.java
@@ -0,0 +1,15 @@
+package com.cars.service;
+
+import com.cars.controller.dto.CarDTO;
+import com.cars.controller.dto.CarDTOResponse;
+
+import java.util.List;
+
+public interface ICarService {
+    CarDTOResponse findById(Long id);
+    List<CarDTOResponse> findAll();
+    CarDTOResponse createCar(CarDTO carDTO);
+    CarDTOResponse findByModel(String model);
+    CarDTOResponse updateCar(CarDTO carDTO,Long id);
+    void deleteCar(Long id);
+}
