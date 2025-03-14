e9372442e99be1b8b4b1c0e5a0996acc0a9ae8d6
Andre <lulesaaso2@gmail.com>
2025-03-14 11:30:05 -0500

    add(findByModel)-findByModel-repo

--- src/main/java/com/cars/repo/CarRepo.java
+++ src/main/java/com/cars/repo/CarRepo.java
@@ -4,6 +4,9 @@ import com.cars.persistence.CarEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CarRepo extends JpaRepository<CarEntity,Long> {
+    Optional<CarEntity> findByModel(String model);
 }
