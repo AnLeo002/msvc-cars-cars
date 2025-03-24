c60842f353eaeccab2756c119d2986cf59f360b8
Andre <lulesaaso2@gmail.com>
2025-03-24 13:23:44 -0500

    update(id -> plate)-CarDTO-controller/dto

--- src/main/java/com/cars/controller/dto/CarDTO.java
+++ src/main/java/com/cars/controller/dto/CarDTO.java
@@ -3,10 +3,15 @@ package com.cars.controller.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 import java.math.BigDecimal;
 
-public record CarDTO(@NotBlank int age,
+public record CarDTO(@NotBlank
+                     @Pattern(regexp = "^[A-Z]{3}[0-9]{3}$|^[A-Z]{3}[0-9]{2}[A-Z]$",
+                             message = "La placa debe tener el formato BDP018 o DGF87G")
+                     String plate,
+                     @NotBlank int age,
                      @NotBlank double km,
                      @NotBlank String color,
                      @NotBlank String description,
