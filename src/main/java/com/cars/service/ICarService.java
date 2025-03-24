596aedd9e80d62d654c0145e3ce3d0e8c94bca9f
Andre <lulesaaso2@gmail.com>
2025-03-24 13:24:23 -0500

    update(id -> plate)-ICarService-service

--- src/main/java/com/cars/service/ICarService.java
+++ src/main/java/com/cars/service/ICarService.java
@@ -6,9 +6,9 @@ import com.cars.controller.dto.CarDTOResponse;
 import java.util.List;
 
 public interface ICarService {
-    CarDTOResponse findById(Long id);
+    CarDTOResponse findById(String plate);
     List<CarDTOResponse> findAll();
     CarDTOResponse createCar(CarDTO carDTO);
-    CarDTOResponse updateCar(CarDTO carDTO,Long id);
-    void deleteCar(Long id);
+    CarDTOResponse updateCar(CarDTO carDTO,String plate);
+    void deleteCar(String plate);
 }
