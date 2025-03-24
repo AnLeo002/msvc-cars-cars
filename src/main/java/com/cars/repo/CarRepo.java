72b9b47d601432dc364f86f5d158024368617d76
Andre <lulesaaso2@gmail.com>
2025-03-24 13:26:40 -0500

    update(Long -> String)-CarRepo-repo

--- src/main/java/com/cars/repo/CarRepo.java
+++ src/main/java/com/cars/repo/CarRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CarRepo extends JpaRepository<CarEntity,Long> {
+public interface CarRepo extends JpaRepository<CarEntity,String> {
 }
