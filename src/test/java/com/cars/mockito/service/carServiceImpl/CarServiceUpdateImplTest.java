a853260125537352a536d535ac5be0192a215bf2
Andre <lulesaaso2@gmail.com>
2025-05-15 12:34:37 -0500

    create-CarServiceUpdateImplTest-service/carServiceImpl

---
+++ src/test/java/com/cars/mockito/service/carServiceImpl/CarServiceUpdateImplTest.java
@@ -0,0 +1,89 @@
+package com.cars.mockito.service.carServiceImpl;
+
+import com.cars.controller.dto.CarDTO;
+import com.cars.controller.dto.CarDTOResponse;
+import com.cars.mockito.DataProvider;
+import com.cars.persistence.CarEntity;
+import com.cars.repo.*;
+import com.cars.service.impl.CarServiceImpl;
+import com.cars.service.impl.CarValidationService;
+import com.cars.service.record.CarValidateComponents;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class CarServiceUpdateImplTest {
+    @Mock
+    private CarRepo carRepo;
+    @Mock
+    private ModelMapper modelMapper;
+    @Mock
+    private CarValidationService carValidationService;
+    @InjectMocks
+    private CarServiceImpl carService;
+    CarEntity testCar;
+    CarDTO carDTO;
+    CarValidateComponents components;
+    @BeforeEach
+    void setup(){
+        testCar = DataProvider.createTestCar("BDP018");
+        carDTO = DataProvider.createTestCarDTO("BDP018","nissan");
+        components = new CarValidateComponents(
+                DataProvider.createTestFuel(),
+                DataProvider.createTestTrans(),
+                DataProvider.createTestCarType(),
+                DataProvider.createTestModel(),
+                DataProvider.createTestVersion());
+    }
+    @Test
+    void testUpdateCar(){
+        //When
+        when(carRepo.findById(carDTO.plate())).thenReturn(Optional.of(testCar));
+        when(carRepo.save(any(CarEntity.class))).thenReturn(testCar);
+        when(carValidationService.verifiedCarDTO(carDTO)).thenReturn(components);
+        when(modelMapper.map(testCar, CarDTOResponse.class)).
+                thenReturn(DataProvider.createTestCarMapResponse(testCar));
+        //Then
+        CarDTOResponse result = carService.updateCar(carDTO);
+        assertNotNull(result);
+        assertEquals("BDP018",result.plate());
+        assertEquals("nissan",result.brand());
+        // Verificaciones de interacción correctas
+        verify(carRepo, times(1)).findById(carDTO.plate());
+        verify(carRepo, times(1)).save(any(CarEntity.class));
+    }
+    @Test
+    void testUpdateCarPlateExist(){
+        //When
+        when(carRepo.findById(carDTO.plate())).thenReturn(Optional.empty());
+        assertThrows(ResponseStatusException.class,()->{
+            carService.updateCar(carDTO);
+        });
+    }
+    @Test
+    void testUpdateCarCatch() {
+        //When
+        when(carRepo.findById(carDTO.plate())).thenReturn(Optional.of(testCar));
+        when(carRepo.save(any())).thenReturn(any());
+        when(carValidationService.verifiedCarDTO(carDTO)).thenReturn(components);
+        when(modelMapper.map(testCar, CarDTOResponse.class)).
+                thenReturn(DataProvider.createTestCarMapResponse(testCar));
+        //Then
+        assertThrows(ResponseStatusException.class,()->{
+            carService.updateCar(carDTO);
+        });
+    }
+}
