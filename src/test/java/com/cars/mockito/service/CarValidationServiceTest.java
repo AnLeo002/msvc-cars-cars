e28619781aa09f6730b98ef71c2bc2423c4305aa
Andre <lulesaaso2@gmail.com>
2025-05-15 21:13:49 -0500

    create-CarValidationService-service

---
+++ src/test/java/com/cars/mockito/service/CarValidationServiceTest.java
@@ -0,0 +1,163 @@
+package com.cars.mockito.service;
+
+import com.cars.controller.dto.CarDTO;
+import com.cars.mockito.DataProvider;
+import com.cars.repo.*;
+import com.cars.service.impl.CarValidationService;
+import com.cars.service.record.CarValidateComponents;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class CarValidationServiceTest {
+    @Mock
+    private TransmissionRepo transmissionRepo;
+    @Mock
+    private FuelTypeRepo fuelTypeRepo;
+    @Mock
+    CarTypeRepo carTypeRepo;
+    @Mock
+    private ModelRepo modelRepo;
+    @InjectMocks
+    private CarValidationService carValidationService;
+    private CarDTO carDTO;
+    @BeforeEach
+    void setUp(){
+        carDTO = DataProvider.createTestCarDTO("BDP018","nissan");
+    }
+    @Test
+    void testVerifiedCarDTO(){
+        //When
+        when(transmissionRepo.findByTransmissionIgnoreCaseAndSpeeds(
+                carDTO.transmission().transmission(),
+                carDTO.transmission().speeds()))
+                .thenReturn(Optional.of(DataProvider.createTestTrans()));
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.of(DataProvider.createTestFuel()));
+        when(carTypeRepo.findByTypeIgnoreCase(carDTO.type())).thenReturn(
+                Optional.of(DataProvider.createTestCarType()));
+        when(modelRepo.findByModelIgnoreCase(carDTO.model())).thenReturn(
+                Optional.of(DataProvider.createTestModel()));
+        //Then
+        CarValidateComponents result = carValidationService.verifiedCarDTO(carDTO);
+        assertNotNull(result);
+        assertEquals("corriente",result.fuelType().getFuel());
+        assertEquals("nissan",result.model().getBrand().getBrand());
+        // Verificaciones de interacción correctas
+        verify(modelRepo, times(1)).findByModelIgnoreCase(carDTO.model());
+    }
+
+    @Test
+    void testCreateCarFuelNoExist(){
+        //When
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.empty());
+        //Given
+        assertThrows(ResponseStatusException.class,()->{
+            carValidationService.verifiedCarDTO(carDTO);
+        });
+    }
+    @Test
+    void testCreateCarTransNoExist(){
+        //When
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.of(DataProvider.createTestFuel()));
+        when(transmissionRepo.findByTransmissionIgnoreCaseAndSpeeds(
+                carDTO.transmission().transmission(),
+                carDTO.transmission().speeds()))
+                .thenReturn(Optional.empty());
+        //Given
+        assertThrows(ResponseStatusException.class,()->{
+            carValidationService.verifiedCarDTO(carDTO);
+        });
+    }
+    @Test
+    void testCreateCarTypeNoExist() {
+        //When
+        when(transmissionRepo.findByTransmissionIgnoreCaseAndSpeeds(
+                carDTO.transmission().transmission(),
+                carDTO.transmission().speeds()))
+                .thenReturn(Optional.of(DataProvider.createTestTrans()));
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.empty());
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.of(DataProvider.createTestFuel()));
+        when(carTypeRepo.findByTypeIgnoreCase(carDTO.type())).thenReturn(
+                Optional.empty());
+        //Given
+        assertThrows(ResponseStatusException.class,()->{
+            carValidationService.verifiedCarDTO(carDTO);
+        });
+    }
+    @Test
+    void testModelNoExist(){
+        //When
+        when(transmissionRepo.findByTransmissionIgnoreCaseAndSpeeds(
+                carDTO.transmission().transmission(),
+                carDTO.transmission().speeds()))
+                .thenReturn(Optional.of(DataProvider.createTestTrans()));
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.of(DataProvider.createTestFuel()));
+        when(carTypeRepo.findByTypeIgnoreCase(carDTO.type())).thenReturn(
+                Optional.of(DataProvider.createTestCarType()));
+        when(modelRepo.findByModelIgnoreCase(carDTO.model())).thenReturn(
+                Optional.empty());
+        //Given
+        assertThrows(ResponseStatusException.class,()->{
+            carValidationService.verifiedCarDTO(carDTO);
+        });
+    }
+    @Test
+    void testVersionNoExist(){
+        //When
+        when(transmissionRepo.findByTransmissionIgnoreCaseAndSpeeds(
+                carDTO.transmission().transmission(),
+                carDTO.transmission().speeds()))
+                .thenReturn(Optional.of(DataProvider.createTestTrans()));
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.empty());
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.of(DataProvider.createTestFuel()));
+        when(carTypeRepo.findByTypeIgnoreCase(carDTO.type())).thenReturn(
+                Optional.of(DataProvider.createTestCarType()));
+        when(modelRepo.findByModelIgnoreCase(carDTO.model())).thenReturn(
+                Optional.of(DataProvider.createTestModelVersionEmpty()));
+        //Given
+        assertThrows(ResponseStatusException.class,()->{
+            carValidationService.verifiedCarDTO(carDTO);
+        });
+    }
+    @Test
+    void testBrandDifferentToDto(){
+        carDTO = DataProvider.createTestCarDTO("BDP018","ferrari");
+        //When
+        when(transmissionRepo.findByTransmissionIgnoreCaseAndSpeeds(
+                carDTO.transmission().transmission(),
+                carDTO.transmission().speeds()))
+                .thenReturn(Optional.of(DataProvider.createTestTrans()));
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.empty());
+        when(fuelTypeRepo.findByFuelIgnoreCase(carDTO.fuel()))
+                .thenReturn(Optional.of(DataProvider.createTestFuel()));
+        when(carTypeRepo.findByTypeIgnoreCase(carDTO.type())).thenReturn(
+                Optional.of(DataProvider.createTestCarType()));
+        when(modelRepo.findByModelIgnoreCase(carDTO.model())).thenReturn(
+                Optional.of(DataProvider.createTestModel()));
+        //Given
+        assertThrows(ResponseStatusException.class,()->{
+            carValidationService.verifiedCarDTO(carDTO);
+        });
+    }
+}
