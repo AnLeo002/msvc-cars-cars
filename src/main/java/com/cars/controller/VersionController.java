7df50159906fc8ddc9ae71f4d712521fcf0e14c5
Andre <lulesaaso2@gmail.com>
2025-06-26 19:55:33 -0500

    add(@Valid)

--- src/main/java/com/cars/controller/VersionController.java
+++ src/main/java/com/cars/controller/VersionController.java
@@ -3,6 +3,7 @@ package com.cars.controller;
 import com.cars.controller.dto.VersionDTO;
 import com.cars.controller.dto.VersionDTOResponse;
 import com.cars.service.IVersionService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -31,11 +32,11 @@ public class VersionController {
         return ResponseEntity.ok(service.findByVersion(version));
     }
     @PostMapping("/create")
-    public ResponseEntity<VersionDTOResponse> createVersion(@RequestBody VersionDTO versionDTO){
+    public ResponseEntity<VersionDTOResponse> createVersion(@RequestBody @Valid VersionDTO versionDTO){
         return new ResponseEntity<>(service.createVersion(versionDTO), HttpStatus.CREATED);
     }
     @PutMapping("/update/{id}")
-    public ResponseEntity<VersionDTOResponse> updateVersion(@RequestBody VersionDTO versionDTO, @PathVariable Long id){
+    public ResponseEntity<VersionDTOResponse> updateVersion(@RequestBody @Valid VersionDTO versionDTO, @PathVariable Long id){
         return ResponseEntity.ok(service.updateVersion(versionDTO,id));
     }
     @DeleteMapping("/delete/{id}")
